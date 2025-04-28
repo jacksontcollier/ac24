894af2634472b41bf063747b659b78efe7bcace4
Jackson Collier <jacksontcollier@gmail.com>
2025-04-28 18:08:10 -0400

    Update

--- day09/solution.cpp
+++ day09/solution.cpp
@@ -27,6 +27,8 @@ void print_fileblocks(const vector<int>& fileblocks);
 
 void compact(vector<int>& fileblocks);
 
+vector<int> compact2(const string& diskmap);
+
 unsigned long long calc_checksum(const vector<int>& fileblocks);
 
 int main(int argc, char** argv)
@@ -55,83 +57,14 @@ int main(int argc, char** argv)
     unsigned long long checksum = calc_checksum(fileblocks);
     cout << "Part 1 Answer: Checksum is " << checksum << "\n";
 
-    vector<shared_ptr<FileBlock>> file_blocks;
-    map<size_t, shared_ptr<FreeBlock>> free_blocks;
-
-    bool is_reading_file_block = true;
-    size_t file_block_id = 0;
-    size_t location = 0;
-
-    for (size_t i = 0; i < diskmap.size(); i++) {
-        if (is_reading_file_block) {
-            shared_ptr<FileBlock> fb(new FileBlock);
-            fb->id = file_block_id;
-            fb->location = location;
-            fb->size = diskmap[i] - '0';
-            file_block_id++;
-            location += fb->size;
-            file_blocks.push_back(fb);
-        } else {
-            shared_ptr<FreeBlock> free_block(new FreeBlock);
-            free_block->location = location;
-            free_block->size = diskmap[i] - '0';
-            free_blocks.insert(make_pair(free_block->location, free_block));
-            location += free_block->size;
-        }
-
-        is_reading_file_block = !is_reading_file_block;
-    }
-
-    cout << "Free Blocks\n";
-    cout << "===========\n";
-    for (auto& mit : free_blocks) {
-        size_t location = mit.first;
-        cout << "Location: " << location << ", Size: " << mit.second->size << "\n";
-    }
-
+    vector<int> compact2_fileblocks = compact2(diskmap);
     cout << "\n";
-    cout << "File Blocks\n";
-    cout << "===========\n";
-    for (size_t i = 0; i < file_blocks.size(); i++) {
-        cout << "Location: " << file_blocks[i]->location << ", ID: "
-             << file_blocks[i]->id << ", Size: " << file_blocks[i]->size << "\n";
+    cout << "Part 2 FileBlocks\n";
+    cout << "=================\n";
+    for (size_t i = 0; i < compact2_fileblocks.size(); i++) {
+        cout << compact2_fileblocks[i] << " ";
     }
-
-    map<size_t, shared_ptr<FileBlock>> relocated_file_blocks;
-    for (signed long i = file_blocks.size() - 1; i >= 0; i--) {
-        shared_ptr<FileBlock> file_block = file_blocks[i];
-        bool found_file_block = false;
-        for (auto mit = free_blocks.begin(); mit != free_blocks.end(); mit++) {
-            shared_ptr<FreeBlock> free_block = mit->second;
-            if (free_block->size >= file_block->size) {
-                // Found new location for file block
-                shared_ptr<FileBlock> new_file_block(new FileBlock());
-                new_file_block->id = file_block->id;
-                new_file_block->location = free_block->location;
-                new_file_block->size = file_block->size;
-                relocated_file_blocks.insert(make_pair(new_file_block->location, new_file_block));
-                free_blocks.erase(mit);
-                if (free_block->size > file_block->size) {
-                    shared_ptr<FreeBlock> new_free_block(new FreeBlock());
-                    new_free_block->size = free_block->size - file_block->size;
-                    new_free_block->location = free_block->location + file_block->size;
-                    free_blocks.insert(make_pair(new_free_block->location, new_free_block));
-                }
-                found_file_block = true;
-                break;
-            }
-        }
-        if (!found_file_block) {
-            relocated_file_blocks.insert(make_pair(file_block->location, file_block));
-        }
-    }
-
-    cout << "\nAfter relocation:\n";
-    cout << "===================\n";
-    for (auto& mit : relocated_file_blocks) {
-        cout << "Location: " << mit.first << ", ID: " << mit.second->id << ", Size: "
-             << mit.second->size << "\n";
-    }
+    cout << "\n";
 
     return 0;
 }
@@ -213,11 +146,106 @@ void compact(vector<int>& fileblocks)
     }
 }
 
-void p2_compact(vector<int>& fileblocks)
+vector<int> compact2(const string& diskmap)
 {
-    // Map continguous free block start location to its size (number of contiguous free blocks)
-    map<size_t, size_t> free_section_2_size;
+    vector<FileBlock> file_blocks;
+    map<size_t, FreeBlock> free_blocks;
+    vector<int> compact_file_blocks;
+    bool is_reading_file_block = true;
+    size_t file_block_id = 0;
+    size_t location = 0;
 
+    for (size_t i = 0; i < diskmap.size(); i++) {
+        if (is_reading_file_block) {
+            FileBlock fb;
+            fb.id = file_block_id;
+            fb.location = location;
+            fb.size = diskmap[i] - '0';
+            file_block_id++;
+            location += fb.size;
+            file_blocks.push_back(fb);
+            for (size_t i = 0; i < fb.size; i++) {
+                compact_file_blocks.push_back(0);
+            }
+        } else {
+            FreeBlock free_block;
+            free_block.location = location;
+            free_block.size = diskmap[i] - '0';
+            free_blocks.insert(make_pair(free_block.location, free_block));
+            location += free_block.size;
+            for (size_t i = 0; i < free_block.size; i++) {
+                compact_file_blocks.push_back(0);
+            }
+        }
+
+        is_reading_file_block = !is_reading_file_block;
+    }
+
+    /*
+    cout << "Free Blocks\n";
+    cout << "===========\n";
+    for (auto& mit : free_blocks) {
+        size_t location = mit.first;
+        cout << "Location: " << location << ", Size: " << mit.second->size << "\n";
+    }
+
+    cout << "\n";
+    cout << "File Blocks\n";
+    cout << "===========\n";
+    for (size_t i = 0; i < file_blocks.size(); i++) {
+        cout << "Location: " << file_blocks[i]->location << ", ID: "
+             << file_blocks[i]->id << ", Size: " << file_blocks[i]->size << "\n";
+    }
+    */
+
+    map<size_t, FileBlock> relocated_file_blocks;
+    for (signed long i = file_blocks.size() - 1; i >= 0; i--) {
+        FileBlock& file_block = file_blocks[i];
+        bool found_file_block = false;
+        for (auto mit = free_blocks.begin(); mit != free_blocks.end(); mit++) {
+            FreeBlock& free_block = mit->second;
+            if (free_block.size >= file_block.size) {
+                // Found new location for file block
+                FileBlock new_file_block;
+                new_file_block.id = file_block.id;
+                new_file_block.location = free_block.location;
+                new_file_block.size = file_block.size;
+                relocated_file_blocks.insert(make_pair(new_file_block.location, new_file_block));
+                free_blocks.erase(mit);
+                if (free_block.size > file_block.size) {
+                    FreeBlock new_free_block;
+                    new_free_block.size = free_block.size - file_block.size;
+                    new_free_block.location = free_block.location + file_block.size;
+                    free_blocks.insert(make_pair(new_free_block.location, new_free_block));
+                }
+                FreeBlock new_free_block(FreeBlock());
+                new_free_block->size = file_block.size;
+                new_free_block->location = file_block.location;
+                free_blocks.insert(make_pair(new_free_block.location, new_free_block));
+                found_file_block = true;
+                break;
+            }
+        }
+        if (!found_file_block) {
+            relocated_file_blocks.insert(make_pair(file_block->location, file_block));
+        }
+    }
+
+    for (auto relocated_file_blocks_iter : relocated_file_blocks) {
+        shared_ptr<FileBlock> file_block = relocated_file_blocks_iter.second;
+        for (size_t i = file_block->location; i < (file_block->location + file_block->size); i++) {
+            compact_file_blocks[i] = file_block->id;
+        }
+    }
+
+    for (auto free_block_iter : free_blocks) {
+        shared_ptr<FreeBlock> free_block = free_block_iter.second;
+        for (size_t i = free_block->location; i < (free_block->location + free_block->size); i++) {
+            compact_file_blocks[i] = -1;
+        }
+    }
+
+    return compact_file_blocks;
 }
 
 unsigned long long calc_checksum(const vector<int>& fileblocks)
